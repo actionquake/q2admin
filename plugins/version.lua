b4d6b2bd8fdee2e37c8147cf20b46a934381dfab
m4son <mrsima@gmail.com>
2021-08-09 14:25:30 +0200

    rewrite from scratch

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,63 +1,36 @@
 --[[
 simple !version* script by TgT 
-TODO
-	if version table is empty dont crash -- not yet
-CHANGELOG
-v1.5
-+ MVDSPEC crash fix
-v1.4
-+ prevent !q2client abuse
-v1.3
-+ alternative for !version !versionsay !versionsayall
-v1.2
-+ different way to report a version, works with any client (i hope)
-+ any word that starts with !version reports a client version, not just 3 configured ones
-v1.1
-+ added three common responds for version ( !version !versionsay !versionsayall)
-v1
-+ first attempt to simulate !version with q2admin script
-+ works with q2pro clients
-]]--
 
-local version = "1.5"
+	Requirements: 
+		q2proded r548 or later
+		
+	Changelog:
+		v2.0
+		+ a rewrite from scratch
+	
+--]]
+
+local version = "2.0"
+local word = "!versio.+"
+
 gi.AddCommandString("sets q2a_version "..version.."\n")
-
-local ver_req = {"!versio.+"}
-local clver = {}
-
-function ClientBegin(client)
-    local plr = ex.players[client]
-    allowed = 1
-    ex.stuffcmd(client,"!q2client $version")
-end
-
-local sm = string.match
+gi.AddCommandString("addstuffcmd connect setu version $${version}")
 
 function ClientCommand(client)
-    local cmd = gi.argv(1)
--- 
-    if allowed ~= nil and cmd == "!q2client" then
-        local cver = ""
-        for i=2,gi.argc() do
-            cver = cver .. gi.argv(i) .. " " 
-        end
-
-        clver[client] = cver
-        allowed = nil
-        return true
-    end
--- version stuff
-	for k,v in pairs(ver_req) do
-		word = sm(cmd,v)	    
-		if word then
-			gi.cprintf(client, PRINT_HIGH, 'num  name             client version\n')
-			gi.cprintf(client, PRINT_HIGH, '---  ---------------  ---------------------------------------------\n')
+	local sm = string.match
+	
+	cmd = gi.argv(1)
+	match = sm(cmd,word)
+	
+	if match then
+		gi.cprintf(client,PRINT_HIGH,"num name            client version\n")
+		gi.cprintf(client,PRINT_HIGH,"--- --------------- ---------------------------------------------\n")
+		
 		for i,plr in pairs(ex.players) do
-				if plr.name ~= "[MVDSPEC]" then
-			               gi.cprintf(client, PRINT_HIGH, "%3d  %-15s  %s\n",i, plr.name,clver[i])
-				end
-	  		end
-		return true
+			if plr.name ~= "[MVDSPEC]" then
+				gi.cprintf(client,PRINT_HIGH,"%3d %-15s %s\n", i-1,plr.name,plr.version)
+			end
 		end
+		return true
 	end
 end
