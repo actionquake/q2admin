e024846e339cc105255c84df5f949884ff78be62
m4son <mrsima@gmail.com>
2022-04-06 13:16:58 +0200

    Update config.ex.lua

--- config.ex.lua
+++ config.ex.lua
@@ -21,5 +21,6 @@ plugins = {
     coinflip = {}, -- Heads & Tails script
     broadcast = {}, -- Broadcast script
     version = {}, -- version feedback script
-    mvd2cprints = {} -- store centerprints from AQ2 into MVD2 
+    mvd2cprints = {}, -- store centerprints from AQ2 into MVD2
+    scoreboardStore = {}, -- store scoreboard at match end into MVD2
 }
