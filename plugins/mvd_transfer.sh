740ef42ee5c8ab7b27e81d4c46c4f73a4c86f1d2
Dino <8dino2@gmail.com>
2022-06-12 18:23:11 -0400

    Updated mvd_transfer.sh

--- plugins/mvd_transfer.sh
+++ plugins/mvd_transfer.sh
@@ -3,9 +3,11 @@
 # small transfer script by Shaque 2012-11-22
 # and more changes done by lots of people over the course ;)
 mvd="$1"
-method="$2"
+#method="$2"
 
-_file="action/demos/${mvd}"
+_filepath="action/demos"
+_file="${mvd}"
+_fullfilepath="${_filepath}/${_file}"
 
 # If using s3cmd to upload, set this as your bucket name
 _s3bucket="aq2world"
@@ -20,28 +22,45 @@ _targetdir="demos/aqtion"
 _servertargetdir="SERVERTARGETDIR"
 
 # if using scp to upload, set this as your private key file for PKI auth
-_rsakeylocation="~/.ssh/id_rsa"
+_rsakeylocation="$HOME/.ssh/id_rsa"
 
-if [ -f "$_file" ]; then
+## AQTion MVD transfer
+
+if [ -f "${_fullfilepath}" ]; then
   sleep 2
-  # sleep a little, so q2proded has time to finalize the demo file
-  if [ ${method} == "sftp" ]; then
-    ( echo "put ${_file} '${_targetdir}'" | sftp -q -o PasswordAuthentication=no -p ${_targetuserhost} ) &
-  elif [ ${method} == "scp" ]; then
-    scp -i ${_rsakeylocation} ${_file} ${_targetuserhost} &
-  elif [ ${method} == "s3cmd" ]; then
-    s3cmd put $_file s3://${_s3bucket}/${_targetdir}/${_servertargetdir}/${_file} &
-  elif [ ${method} == "cp" ]; then
-    cp ${_file} ${_targetdir} &
-  elif [ -z ${_file} ]; then
-    echo "$0 Error: '$_file' not found. Not transferring anything. :("
-  else
-    echo "The second argument to this script is your upload method,"
-    echo "choose [ sftp | scp | s3cmd | cp ]"
-    echo "And set your mvd_transfer.sh script correctly!"
-    exit 1
-  fi
+  s3cmd put "${_fullfilepath}" s3://${_s3bucket}/${_targetdir}/${_servertargetdir}/${_file} &
+  sleep 5
+  exit 0
+elif [ -z "${_file}" ]; then
+  echo "$0 Error: $_file not found. Not transferring anything. :("
+  exit 1
+else
+  echo "Something went wrong other than file not found"
+  exit 1
 fi
+
+## Old method, keeping around but unused:
+
+# if [ -f "${_filepath}/${_file}" ]; then
+#   sleep 2
+#   # sleep a little, so q2proded has time to finalize the demo file
+#   if [ "${method}" == "sftp" ]; then
+#     ( echo "put ${_file} '${_targetdir}'" | sftp -q -o PasswordAuthentication=no -p ${_targetuserhost} ) &
+#   elif [ "${method}" == "scp" ]; then
+#     scp -i "${_rsakeylocation}" "${_file}" ${_targetuserhost} &
+#   elif [ "${method}" == "s3cmd" ]; then
+#     s3cmd put "$_file" s3://${_s3bucket}/${_targetdir}/${_servertargetdir}/"${_file}" &
+#   elif [ "${method}" == "cp" ]; then
+#     cp "${_file}" ${_targetdir} &
+#   elif [ -z "${_file}" ]; then
+#     echo "$0 Error: '$_file' not found. Not transferring anything. :("
+#   else
+#     echo "The second argument to this script is your upload method,"
+#     echo "choose [ sftp | scp | s3cmd | cp ]"
+#     echo "And set your mvd_transfer.sh script correctly!"
+#     exit 1
+#   fi
+# fi
 
 
 # vim: expandtab tabstop=2 autoindent:
