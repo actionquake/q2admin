cfac5f9df35f6e48627bfc13042b9ec0f0c315e7
Aaron Dean <8dino2@gmail.com>
2022-07-11 19:34:01 -0400

    Update config.ex.lua
    Added roundlimit to config lua

--- config.ex.lua
+++ config.ex.lua
@@ -15,7 +15,7 @@ plugins = {
             -- mod
             'teamplay', 'ctf', 'matchmode', 'roundtimelimit', 'tgren', 'limchasecam', 'forcedteamtalk',
             'mm_forceteamtalk', 'ir', 'wp_flags', 'itm_flags', 'hc_single', 'use_punch',  'darkmatch',
-            'allitem', 'allweapon', 'use_3teams'
+            'allitem', 'allweapon', 'use_3teams', 'roundlimit'
         }
        },
         mvd = {
